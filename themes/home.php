bbc913d21e1c3fba69e02ebdb458a48586be0879
nguyenquy42 <nguyenquy26012014@gmail.com>
2021-03-18 16:18:41 +0700

    up first

---
+++ themes/home.php
@@ -0,0 +1,18 @@
+<main>
+  <div class="container">
+    <div class="row">
+      <div class="col-4">
+        <div class="b1"></div>
+      </div>
+      <div class="col-4">
+        <div class="">
+          <i class="fi fi-rr-following"></i>
+          <span class="fi fi-rr-gym">
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="b1"></div>
+      </div>
+    </div>
+  </div>
+</main>
